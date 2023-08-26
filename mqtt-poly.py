b9bd58b82a2bf9ffc7ae6eb75882c7d5d73a030f
kevin Graff <kevin@kevin-graff.com>
2023-08-26 18:15:48 -0400

    Update mqtt-poly.py

--- mqtt-poly.py
+++ mqtt-poly.py
@@ -267,7 +267,7 @@ class Controller(udi_interface.Node):
         payload = message.payload.decode("utf-8")
         if self.topic.find("INFO1") > 0:
             LOGGER.info("payload is {}".format(payload))  # 8888888888 need to see what the payload looks like
-            LOGGER.debug("Received {} from {}".format(payload, topic))
+            LOGGER.info("Received {} from {}".format(payload, topic))
 
         try:
             self.poly.getNode(self._dev_by_topic(topic)).updateInfo(payload, topic)
